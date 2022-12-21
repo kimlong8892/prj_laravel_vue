8418fb9fea89308f3687e12b778f152229b0432b
Long Nguyen <long@maruweb.vn>
2022-12-21 17:00:08 +0700

    change AdminStore to AdminLoginStore

---
+++ app/vue/src/store/AdminLogin/store.js
@@ -0,0 +1,13 @@
+import AdminActions from "@/store/AdminLogin/actions";
+import AdminGetTers from "@/store/AdminLogin/getTers";
+import AdminMutations from "@/store/AdminLogin/mutations";
+import AdminStates from "@/store/AdminLogin/states";
+
+const AdminLoginStore = {
+    state: AdminStates,
+    mutations: AdminMutations,
+    getters: AdminGetTers,
+    actions: AdminActions
+};
+
+export default AdminLoginStore;
--- app/vue/src/store/index.js
+++ app/vue/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import AdminStore from "@/store/Admin/store";
+import AdminLoginStore from "@/store/AdminLogin/store";
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
-        AdminStore
+        AdminLoginStore
     }
 });
 
